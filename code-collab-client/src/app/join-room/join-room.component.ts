89c5ddc76bd25d6c0d18b14d8427dcc41a89a944
ahmed <ahmedraslan28@gmail.com>
2025-05-10 18:20:48 +0300

    add angular folder structure

---
+++ code-collab-client/src/app/join-room/join-room.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-room',
+  templateUrl: './join-room.component.html',
+  styleUrls: ['./join-room.component.scss'],
+})
+export class JoinRoomComponent {}
