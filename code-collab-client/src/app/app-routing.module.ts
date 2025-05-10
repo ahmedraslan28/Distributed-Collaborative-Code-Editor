5a8682da0d5e5a987189fbd4c8bf0c85598eefe0
ahmed <ahmedraslan28@gmail.com>
2025-05-10 20:08:59 +0300

    generat join room page and room service

--- code-collab-client/src/app/app-routing.module.ts
+++ code-collab-client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JoinRoomComponent } from './join-room/join-room.component';
+import { EditorComponent } from './editor/editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: JoinRoomComponent },
+  { path: 'editor/:roomId/:username', component: EditorComponent },
+  { path: '**', redirectTo: '' } // Redirect to join room for any unknown routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
