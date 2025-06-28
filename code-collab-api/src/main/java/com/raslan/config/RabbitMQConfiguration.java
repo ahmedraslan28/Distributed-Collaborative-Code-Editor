a12bc828e332b696f39bb3ac8dd932c54f064eec
Ahmed Raslan <ahmedraslan28@gmail.com>
2025-06-28 20:35:15 +0300

    configure rabbitmq

---
+++ code-collab-api/src/main/java/com/raslan/config/RabbitMQConfiguration.java
@@ -0,0 +1,17 @@
+package com.raslan.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+    @Value("${rabbitmq.queue.name}")
+    private String queueName ;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queueName);
+    }
+}
