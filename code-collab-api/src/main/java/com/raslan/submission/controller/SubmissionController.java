8a03d6b55ee66192914493ccbc935d6b97ce63ba
Ahmed Raslan <ahmedraslan28@gmail.com>
2025-06-29 17:58:22 +0300

    remove submission request and add map<String,String>

--- code-collab-api/src/main/java/com/raslan/submission/controller/SubmissionController.java
+++ code-collab-api/src/main/java/com/raslan/submission/controller/SubmissionController.java
@@ -1,19 +1,22 @@
 package com.raslan.submission.controller;
 
-import com.raslan.submission.dto.Submission;
 import com.raslan.submission.service.SubmissionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/submit")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
+@Slf4j
 public class SubmissionController {
     private final SubmissionService submissionService;
 
     @PostMapping
-    public void handleSubmission(@RequestBody Submission request) {
+    public void handleSubmission(@RequestBody Map<String, String> request) {
         submissionService.handleSubmission(request);
     }
 }
--- code-collab-api/src/main/java/com/raslan/submission/service/SubmissionService.java
+++ code-collab-api/src/main/java/com/raslan/submission/service/SubmissionService.java
@@ -1,11 +1,12 @@
 package com.raslan.submission.service;
 
-import com.raslan.submission.dto.Submission;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +16,7 @@ public class SubmissionService {
     @Value("${rabbitmq.queue.name}")
     private String queueName;
 
-    public void handleSubmission(Submission submission) {
+    public void handleSubmission(Map<String, String> submission) {
         rabbitTemplate.convertAndSend(queueName, submission);
         log.info("Submission {} sent to RabbitMQ With queue name {}", submission, queueName);
     }
