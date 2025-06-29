70dd5374036e0d07b47ca18ba7ea70cd0909a356
Ahmed Raslan <ahmedraslan28@gmail.com>
2025-06-30 00:23:13 +0300

    get output from docker service

--- ExecutionService/src/main/java/com/raslan/ExecutionService/service/ExecutionService.java
+++ ExecutionService/src/main/java/com/raslan/ExecutionService/service/ExecutionService.java
@@ -1,5 +1,6 @@
 package com.raslan.ExecutionService.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,17 @@ import java.util.Map;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ExecutionService {
+    private final DockerService dockerService;
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void handleExecution(Map<String, String> submission) {
         log.info("Received submission Request : {}",submission);
+        String output = dockerService.runCode(
+                submission.get("language"),
+                submission.get("code"),
+                submission.get("input")
+        );
+        log.info("Execution completed, output: {}", output);
     }
 }
